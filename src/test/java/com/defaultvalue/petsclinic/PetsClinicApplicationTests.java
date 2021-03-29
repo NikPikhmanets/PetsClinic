8f60e05dd97a5f0b70dc16fee044ffb3239e9871
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-03-29 10:59:05 +0300

    #71 Create database and project (Spring, Security, Thymeleaf, JPA/Hibernate)
    Create project

---
+++ src/test/java/com/defaultvalue/petsclinic/PetsClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defaultvalue.petsclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
