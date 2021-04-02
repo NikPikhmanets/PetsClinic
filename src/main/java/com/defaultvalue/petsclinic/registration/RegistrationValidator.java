b4c2992a5b81a56489a9c1b72b4a72f335dc2f7d
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-02 17:18:02 +0300

    #113 Implement validation Registration From
    Change some message of errorCode, add Constants

--- src/main/java/com/defaultvalue/petsclinic/registration/RegistrationValidator.java
+++ src/main/java/com/defaultvalue/petsclinic/registration/RegistrationValidator.java
@@ -3,14 +3,32 @@ package com.defaultvalue.petsclinic.registration;
 import com.defaultvalue.petsclinic.user.UserService;
 import com.defaultvalue.petsclinic.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import static com.defaultvalue.petsclinic.registration.ConstantsValidator.*;
+
 @Component
 public class RegistrationValidator implements Validator {
+
+    @Value("Size.userForm.noEmpty")
+    private String noEmpty;
+
+    @Value("Size.userForm.invalidSize")
+    private String invalidSize;
+
+    @Value("Duplicate.userForm.email")
+    private String duplicateEmail;
+
+    @Value("Size.userForm.shortPassword")
+    private String shortPassword;
+
+    @Value("Diff.userForm.passwordConfirm")
+    private String passwordConfirm;
 
     private final UserService userService;
 
@@ -28,25 +46,25 @@ public class RegistrationValidator implements Validator {
     public void validate(Object target, Errors errors) {
         RegistrationForm form = (RegistrationForm) target;
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "Size.userForm.noEmpty");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "surname", "Size.userForm.noEmpty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, FIRST_NAME, noEmpty);
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, SURNAME, noEmpty);
 
         if (form.getEmail() == null || !StringUtils.hasText(form.getEmail())) {
-            errors.rejectValue("email", "Size.userForm.noEmpty");
+            errors.rejectValue(EMAIL, noEmpty);
         } else if (form.getEmail().length() < 2 || form.getEmail().length() > 32) {
-            errors.rejectValue("email", "Size.userForm.size");
+            errors.rejectValue(EMAIL, invalidSize);
         } else if (userService.findUserByEmail(form.getEmail()) != null) {
-            errors.rejectValue("email", "Duplicate.userForm.email");
+            errors.rejectValue(EMAIL, duplicateEmail);
         }
 
         if (form.getPassword() == null || !StringUtils.hasText(form.getPassword())) {
-            errors.rejectValue("password", "Size.userForm.noEmpty");
+            errors.rejectValue(PASSWORD, noEmpty);
         } else if (form.getPassword().length() < 8 || form.getPassword().length() > 32) {
-            errors.rejectValue("password", "Size.userForm.password");
+            errors.rejectValue(PASSWORD, shortPassword);
         }
 
         if (!form.getPasswordConfirm().equals(form.getPassword())) {
-            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+            errors.rejectValue(PASSWORD_CONFIRM, passwordConfirm);
         }
     }
 }
