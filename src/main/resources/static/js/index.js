edbd4cc733932c9a6f3bbd2287da469285d6cc5e
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-07 15:36:24 +0300

    #117 Implement jQuery requests for show data in  index.html
    Implemented block of slider (doctors)

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -6,6 +6,22 @@ function updateSelectInForm(element, data) {
 }
 
 function getDoctors() {
+    function updateSlider(data) {
+        let wrapper = '.slider-wrapper';
+        $(wrapper).children().remove();
+
+        for (const item of data) {
+            $(wrapper).append(
+                '<div class="about-item text-center"> ' +
+                '     <img src="https://nationwide-energy.co.uk/wp-content/uploads/2017/07/blank-avatar.jpg"' +
+                '          alt="void">' +
+                '         <p class="about-name">' + data.name + '</p>' +
+                '         <p class="about-speciality">' + data.email + '</p>' +
+                ' </div>'
+            );
+        }
+    }
+
     $.ajax({
         url: "users/doctors",
         async: true,
@@ -13,6 +29,7 @@ function getDoctors() {
         success: function (data) {
             console.log(data);
             updateSelectInForm('#doctorsFormControlSelect', data);
+            updateSlider(data);
         }
     });
 }
