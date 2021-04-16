5658bb6b8ea326d9c4c4ca12e327e192bdc73853
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-16 18:10:13 +0300

    #133 Implement getting Pet with his list of issues
    Change PetShortInfoDTO: add getting id

--- src/main/java/com/defaultvalue/petsclinic/pet/dto/PetShortInfoDTO.java
+++ src/main/java/com/defaultvalue/petsclinic/pet/dto/PetShortInfoDTO.java
@@ -5,6 +5,8 @@ import org.immutables.value.Value;
 @Value.Immutable
 public abstract class PetShortInfoDTO {
 
+    public abstract Long getId();
+
     public abstract String getName();
 
     public abstract String getKind();
