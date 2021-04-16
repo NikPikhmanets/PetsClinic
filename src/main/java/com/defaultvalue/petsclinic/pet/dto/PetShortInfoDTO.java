06f69e7f6fc6de0f576054cb593fe179bec49ed7
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-16 16:49:50 +0300

    Refactoring: fix link in navbar.html "me" -> "profile"

---
+++ src/main/java/com/defaultvalue/petsclinic/pet/dto/PetShortInfoDTO.java
@@ -0,0 +1,11 @@
+package com.defaultvalue.petsclinic.pet.dto;
+
+import org.immutables.value.Value;
+
+@Value.Immutable
+public abstract class PetShortInfoDTO {
+
+    public abstract String getName();
+
+    public abstract String getKind();
+}
