5b2aad1d5fdd2ddb67fd827e95792b5bd3823f88
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-19 10:53:35 +0300

    #135 Implement getting Issue (by Id, by Doctor, by status, petId)
    Add issueId and implemented build list of IssueDTO in IssueDTO

--- src/main/java/com/defaultvalue/petsclinic/issue/dto/IssueDTO.java
+++ src/main/java/com/defaultvalue/petsclinic/issue/dto/IssueDTO.java
@@ -1,9 +1,32 @@
 package com.defaultvalue.petsclinic.issue.dto;
 
+import com.defaultvalue.petsclinic.issue.entity.Issue;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class IssueDTO {
 
+    private long issueId;
     private String description;
     private long petId;
+
+    public IssueDTO() {
+    }
+
+    private IssueDTO(long issueId, String description, long petId) {
+        this.issueId = issueId;
+        this.description = description;
+        this.petId = petId;
+    }
+
+    public long getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(long issueId) {
+        this.issueId = issueId;
+    }
 
     public String getDescription() {
         return description;
@@ -19,5 +42,11 @@ public class IssueDTO {
 
     public void setPetId(long petId) {
         this.petId = petId;
+    }
+
+    public List<IssueDTO> getListIssueDTO(List<Issue> issues) {
+        return issues.stream()
+                .map(issue -> new IssueDTO(issue.getId(), issue.getDescription(), issue.getPetId()))
+                .collect(Collectors.toList());
     }
 }
