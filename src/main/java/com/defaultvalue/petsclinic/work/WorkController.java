57b45c98f355d6c117a1516d801c33319a63a59d
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-21 18:23:42 +0300

    #139 Implement view for Doctor (with pagination and filter by status)
    Add to WorkController '@Secured(ROLE_DOCTOR)'

--- src/main/java/com/defaultvalue/petsclinic/work/WorkController.java
+++ src/main/java/com/defaultvalue/petsclinic/work/WorkController.java
@@ -1,11 +1,15 @@
 package com.defaultvalue.petsclinic.work;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static com.defaultvalue.petsclinic.issue.IssueController.ROLE_DOCTOR;
+
 @Controller
 @RequestMapping("/works")
+@Secured(ROLE_DOCTOR)
 public class WorkController {
 
     @GetMapping
