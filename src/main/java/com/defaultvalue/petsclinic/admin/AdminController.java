c163b0f0b27544cad34cbf090d00069d687fe2d8
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-26 14:35:13 +0300

    #147 Implement Admin page for set Role of User (set as doctor)
    Implemented AdminController for return view of 'Admin' page

---
+++ src/main/java/com/defaultvalue/petsclinic/admin/AdminController.java
@@ -0,0 +1,17 @@
+package com.defaultvalue.petsclinic.admin;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
+@Secured("ROLE_ADMIN")
+public class AdminController {
+
+    @GetMapping
+    public String pageOfAdmin() {
+        return "admin";
+    }
+}
