8b9be2e2db428cd5094e109ebeec3e037e524e4e
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-27 17:47:21 +0300

    Refactoring: create final variable "ROLE_ADMIN"

--- src/main/java/com/defaultvalue/petsclinic/admin/AdminController.java
+++ src/main/java/com/defaultvalue/petsclinic/admin/AdminController.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static com.defaultvalue.petsclinic.admin.AdminController.ROLE_ADMIN;
+
 @Controller
 @RequestMapping("admin")
-@Secured("ROLE_ADMIN")
+@Secured(ROLE_ADMIN)
 public class AdminController {
+
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
 
     @GetMapping
     public String pageOfAdmin() {
