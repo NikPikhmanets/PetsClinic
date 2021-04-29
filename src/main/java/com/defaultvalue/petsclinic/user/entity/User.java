3fe01e14823dc938f4651c33971abd112e54eb93
Mykola Pikhmanets <nik.pikhmanets@gmail.com>
2021-04-29 18:04:28 +0300

    #152 Write integration test (UserController)
    Changed to FetchType.EAGER for roles in User

--- src/main/java/com/defaultvalue/petsclinic/user/entity/User.java
+++ src/main/java/com/defaultvalue/petsclinic/user/entity/User.java
@@ -40,7 +40,7 @@ public class User {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
